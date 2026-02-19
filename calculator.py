2edc9fc8732a6e76be606b378d251d9ad7b6fb9b
Hannah <hannahjaneuk@yahoo.co.uk>
2026-02-19 16:34:23 +0000

    studying rotation rate

--- calculator.py
+++ calculator.py
@@ -194,7 +194,124 @@ class SRD:
             raise ValueError("grid must be 'all', 'land', or 'ocean'")
 
         
+class Tempdiff:
+    @staticmethod
+    def temp_diff(ds, var, lat_slice):
+        """
+        Compute temperature difference between equator and either north or south pole for:
+        var = "st" or "tsurf"
+        """
 
+        ds = ds.isel(lat=lat_slice)
 
-    
+        # Land mask
+        lsm = ds["lsm"]
+        land = lsm > 0
 
+        # Variable extraction
+        if var == "st":
+            data = ds["st"].isel(soil_layer=0)
+        
+        elif var == "tsurf":
+            data = ds["tsurf"]
+        
+        else:
+            raise ValueError("var must be 'st' or 'tsurf'")
+
+        # Masked time mean
+        mean = data.mean("time")
+        masked_mean = mean.where(land)
+
+        # Weights
+        latitude = masked_mean["lat"]
+        weights = np.cos(np.deg2rad(latitude))
+        weights = weights.where(np.isfinite(weights), 0)
+
+        # Function to compute area-weighted mean over a boolean latitude mask
+        def area_weighted_mean(masked_data,lat_mask):
+            # Latitude mask for the region of interest
+            latitudes = weights.where(lat_mask, 0)
+            latitudes_2d, _ = xr.broadcast(latitudes, masked_data)
+            num = (masked_data * latitudes_2d).sum(dim=["lat", "lon"])
+            den = latitudes_2d.sum(dim=["lat", "lon"])
+            return num / den if den != 0 else 0
+        
+        # Create latitude masks for equator and poles
+        # Equator mask: latitude between -10 and 10 degrees
+        equator_mask = (latitude >= -10) & (latitude <= 10)
+        equator_temp = area_weighted_mean(masked_mean, equator_mask)
+
+        # Polar masks
+        north_pole_mask = latitude >= 80
+        south_pole_mask = latitude <= -80
+
+        # Calculate temperature depending on what user wants
+        if lat_slice == slice(0,48):
+            # Both poles
+            polar_mask = north_pole_mask | south_pole_mask
+        elif lat_slice == slice(0,24):
+            # North pole only
+            polar_mask = north_pole_mask
+        elif lat_slice == slice(24,48):
+            # South pole only
+            polar_mask = south_pole_mask
+        else:
+            raise ValueError("lat slice must be either 0,48, 0,24, or 24,48")
+        
+        polar_temp = area_weighted_mean(masked_mean, polar_mask)
+
+        return float((equator_temp - polar_temp).item()), float(equator_temp), float(polar_temp)
+
+class Tempdiffall:
+    @staticmethod
+    def temp_diff(ds, lat_slice):
+        """
+        Compute temperature difference between equator and either north or south pole for:
+        tsurf over all grid cells (no land mask)
+        """
+
+        ds = ds.isel(lat=lat_slice)
+
+        # Masked time mean
+        data = ds["tsurf"]
+        mean = data.mean("time")
+
+        # Weights
+        latitude = mean["lat"]
+        weights = np.cos(np.deg2rad(latitude))
+        weights = weights.where(np.isfinite(weights), 0)
+
+        # Function to compute area-weighted mean over a boolean latitude mask
+        def area_weighted_mean(masked_data,lat_mask):
+            # Latitude mask for the region of interest
+            latitudes = weights.where(lat_mask, 0)
+            latitudes_2d, _ = xr.broadcast(latitudes, masked_data)
+            num = (masked_data * latitudes_2d).sum(dim=["lat", "lon"])
+            den = latitudes_2d.sum(dim=["lat", "lon"])
+            return num / den if den != 0 else 0
+        
+        # Create latitude masks for equator and poles
+        # Equator mask: latitude between -10 and 10 degrees
+        equator_mask = (latitude >= -10) & (latitude <= 10)
+        equator_temp = area_weighted_mean(mean, equator_mask)
+
+        # Polar masks
+        north_pole_mask = latitude >= 80
+        south_pole_mask = latitude <= -80
+
+        # Calculate temperature depending on what user wants
+        if lat_slice == slice(0,48):
+            # Both poles
+            polar_mask = north_pole_mask | south_pole_mask
+        elif lat_slice == slice(0,24):
+            # North pole only
+            polar_mask = north_pole_mask
+        elif lat_slice == slice(24,48):
+            # South pole only
+            polar_mask = south_pole_mask
+        else:
+            raise ValueError("lat slice must be either 0,48, 0,24, or 24,48")
+        
+        polar_temp = area_weighted_mean(mean, polar_mask)
+
+        return float((equator_temp - polar_temp).item()), float(equator_temp), float(polar_temp)
